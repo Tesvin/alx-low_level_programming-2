c306d2b5aea98ecaa8fedb5c99e3a7db50bb8ceb
stephen <stephenazosike@gmail.com>
2022-07-05 05:57:18 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
  * main - check the code
  * Return: Always 0
  */
 int main(void)
 {
-	char *sh ="";
+	char *sh ="main";
 
 	while (*sh)
 	{
-	_putchar(*sh);
-	sh++;
+		_putchar(*sh);
+		sh++;
 	}
 	_putchar('\n');
 
