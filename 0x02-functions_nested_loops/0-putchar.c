620fe93622fdcecc594e074981e026ad3ffbf260
stephen <stephenazosike@gmail.com>
2022-07-05 04:35:02 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *sh ="";
+
+	while (*sh)
+	{
+	_putchar(*sh);
+	sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
