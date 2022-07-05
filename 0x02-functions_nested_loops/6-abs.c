aee91fbf27d57a3e92e3d17407fd302dec8477ef
stephen <stephenazosike@gmail.com>
2022-07-05 08:08:31 -0700

    cmmit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ *  * _abs - a function that computes the absolute value of an integer
+ */
+int _abs(int ab)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,8 +4,8 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int ab);
 /**
-* int _abs(int ab);
 * int print_last_digit(int nld);
 * void jack_bauer(void);
 * void times_table(void);
