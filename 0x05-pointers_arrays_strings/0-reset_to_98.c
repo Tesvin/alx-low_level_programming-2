913a645c30202484524ed4b2d7931675d72e9cb0
stephen <stephenazosike@gmail.com>
2022-07-12 06:36:18 -0700

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /*
- * reset_to_98 - pointer to an int as parameter and 
+ * reset_to_98 - pointer to an int as parameter and
  * updates the value it points to to 98
  * @n: input
  * Return: n
