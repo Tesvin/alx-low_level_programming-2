80bfde83435b0162bd81098288f75caa984f13bc
stephen <stephenazosike@gmail.com>
2022-07-14 17:35:30 -0700

    commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,7 +4,7 @@
  * write your line of code here...
  * Remember:
  * - you are not allowed to use a
- * - you are not allowed to modify 
+ * - you are not allowed to modify
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
